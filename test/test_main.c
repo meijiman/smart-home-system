b3c58462095c6fe2802c78d35e7cbe9d0db2de38
MeijiMan <meijiman@outlook.com>
2023-12-19 00:22:07 +0700

    add hal

---
+++ test/test_main.c
@@ -0,0 +1,22 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "main.h"
+#include "mock_stm32f1xx_hal.h"
+#include <stdbool.h>
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_testmain(void)
+{
+	TEST_ASSERT_TRUE(true);
+}
+
+#endif // TEST
